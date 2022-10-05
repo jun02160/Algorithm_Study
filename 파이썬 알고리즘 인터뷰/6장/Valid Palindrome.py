9cc98ce674a19f56ee7461079a61b99fbf4f1b97
jun <jun020216@naver.com>
2022-10-05 20:35:44 +0900

    [CH06 문자열 조작] 유효한 팰린드롬

--- 파이썬 알고리즘 인터뷰/6장/Valid Palindrome.py
+++ 파이썬 알고리즘 인터뷰/6장/Valid Palindrome.py
@@ -1,3 +1,8 @@
+import collections
+import re
+from typing import Deque
+
+
 def isPalindrome(s: str) -> bool:
     s = s.lower()
     str_list = []
@@ -5,11 +10,32 @@ def isPalindrome(s: str) -> bool:
         if c.isalnum():
             str_list.append(c)
     for i in range(len(str_list)):
-        print(str_list[i])
-        print(str_list[len(str_list) - i - 1])
+        # print(str_list[i])
+        # print(str_list[len(str_list) - i - 1])
         if str_list[i] != str_list[len(str_list) - i - 1]:
             return False;
     return True;
 
-result = isPalindrome("A man, a plan, a canal: Panama")
-print(result)
+def dequeIsPalindrome(self, s: str) -> bool:
+    # 자료형 데크로 선언
+    strs: Deque = collections.deque()
+
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+
+    while len(strs) > 1:
+        if strs.popleft() != strs.pop():
+            return False
+
+    return True
+
+def slicingIsPalindrome(self, s: str) -> bool:
+    s = s.lower()
+    #정규식을 이용해 불필요한 문자열 필터링
+    s = re.sub('[^a-z0-9]', '', s)
+
+    return s == s[::-1]
+
+print(isPalindrome("A man, a plan, a canal: Panama"))
+print(isPalindrome("race a car"))
