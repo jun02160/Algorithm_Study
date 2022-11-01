3d01127cc407d5209f36e424b21fdf7c52f03b9a
jun <jun020216@naver.com>
2022-11-01 17:35:31 +0900

    [CH07 배열] 자신을 제외한 배열의 곱

--- 파이썬 알고리즘 인터뷰/7장/Product Of Array Except Self.py
+++ 파이썬 알고리즘 인터뷰/7장/Product Of Array Except Self.py
@@ -1,5 +1,19 @@
 # leetcode 문제. https://leetcode.com/problems/product-of-array-except-self/
 # 자신을 제외한 나머지 모든 요소의 곱셈 결과를 출력하는 문제
+from typing import List
+
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = []
+        mul = 1
+        for j in range(0, len(nums)):
+            for i in range(0, len(nums)):
+                if i != j:
+                    mul *= nums[i]
+            result.append(mul)
+            mul = 1
+        return result
 
+a = Solution()
+print(a.productExceptSelf([-1,1,0,-3,3]))
