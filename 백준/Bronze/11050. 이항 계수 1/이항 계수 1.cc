7489b86d614d29b8670deb0c14d8b8465436bcc8
Jun <80024278+jun02160@users.noreply.github.com>
2023-07-31 02:37:42 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int num) {
+    if (num == 1 || num == 0) return 1;
+    else return factorial(num-1) * num;
+}
+
+int main() {
+
+    int n, k;
+    cin >> n >> k;
+    cout << factorial(n) / (factorial(k)*factorial(n-k)) << endl;
+}
