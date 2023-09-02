179a19522f844f15b9e3fc2f60960482c73cf0f4
Jun <80024278+jun02160@users.noreply.github.com>
2023-09-02 16:05:11 +0900

    [Gold II] Title: 문제집, Time: 1036 ms, Memory: 49940 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(stk.nextToken()); // 학생 수 = 그래프의 정점
+        int M = Integer.parseInt(stk.nextToken()); // 그래프의 간선
+
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++)
+            graph.add(new ArrayList<>());
+
+        int[] indegree = new int[N + 1];
+        for (int i = 0; i < M; i++) {
+            stk = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(stk.nextToken());
+            int b = Integer.parseInt(stk.nextToken());
+
+            graph.get(a).add(b);
+            indegree[b]++;
+        }
+
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+        for (int i = 1; i <= N; i++) {
+            if (indegree[i] == 0)
+                que.offer(i);
+        }
+        while (!que.isEmpty()) {
+            int now = que.poll();
+            System.out.print(now + " ");
+            for (int cur : graph.get(now)) {
+                indegree[cur]--;
+                if (indegree[cur] == 0)
+                    que.offer(cur);
+            }
+        }
+    }
+
+}
