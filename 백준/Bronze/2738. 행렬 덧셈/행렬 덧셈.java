bd230307447262f2db1b2ba7ad9704080ef0574d
Yejun Park <80024278+jun02160@users.noreply.github.com>
2024-02-01 15:39:24 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 440 ms, Memory: 22572 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        int[][] arr1 = new int[N][M];
+        
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j=0; j<M; j++) {
+                arr1[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j=0; j<M; j++) {
+                arr1[i][j] += Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<M; j++) {
+                System.out.print(arr1[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+	}
+}
