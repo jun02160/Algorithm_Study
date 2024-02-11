b3f813348c506cda416f3ce01f34e44bdb2abba2
Yejun Park <80024278+jun02160@users.noreply.github.com>
2024-02-11 22:41:41 +0900

    Time: 3 ms (76.43%), Space: 42.8 MB (74.92%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,8 +1,23 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String filteredStr = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-        String reverseStr = new StringBuilder(filteredStr).reverse().toString();
+        int start = 0;
+        int end = s.length()-1;
         
-        return filteredStr.equals(reverseStr);
+        // char 원시형으로 비교하는 방법이 문자열의 동등성을 비교하는 방법보다 빠름!
+        while (start <= end) {
+            if (!Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+            } else if (!Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+            } else {
+                if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+        }
+        
+        return true;
     }
 }
